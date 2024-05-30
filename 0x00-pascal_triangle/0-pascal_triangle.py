a00c54c9f937b75f19edfaedd39b649f3ed9313a
Sameh ElGazar <selgazar95@gmail.com>
2024-05-30 03:27:20 +0300

    pascall

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,23 +1,25 @@
 """Pascal Triangle Implementation"""
 
+
 def pascal_triangle(n):
+    """Pascal Triangle Implementation"""
     pascal = []
-    
+
     if n >= 1:
         pascal = [[1]]
 
     if n >= 2:
-        pascal = [[1],[1, 1]]
+        pascal = [[1], [1, 1]]
 
     if n >= 3:
         i = 2
         for i in range(i, n):
             temp = []
-            m = i + 1;
-            
+            m = i + 1
+
             temp.append(1)
             for j in range(1, m - 1):
-                temp.append(pascal[i - 1][j - 1] + pascal[i - 1][j])            
+                temp.append(pascal[i - 1][j - 1] + pascal[i - 1][j])
             temp.append(1)
             pascal.append(temp)
 
