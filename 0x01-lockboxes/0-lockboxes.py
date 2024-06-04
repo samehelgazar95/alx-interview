d96dd743baab9087f652add9eb52d86c05948ef0
Sameh ElGazar <selgazar95@gmail.com>
2024-06-05 01:38:18 +0300

    0

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,24 +7,24 @@ def canUnlockAll(boxes):
     if len(boxes[0]) == 0:
         return False
 
-    keysSet = set()
-    keysMap = {i: False for i in range(len(boxes))}
-    keysMap[0] = True
+    keys_set = set()
+    keys_map = {i: False for i in range(len(boxes))}
+    keys_map[0] = True
 
-    prevLen = len(keysSet)
-    keysSet.update(boxes[0])
+    prev_len = len(keys_set)
+    keys_set.update(boxes[0])
 
-    while len(keysSet) > prevLen:
-        prevLen = len(keysSet)
+    while len(keys_set) > prev_len:
+        prev_len = len(keys_set)
         new_keys = []
-        for key in keysSet:
-            keysMap[key] = True
+        for key in keys_set:
+            keys_map[key] = True
             new_keys.extend(boxes[key])
 
-        keysSet.update(new_keys)
+        keys_set.update(new_keys)
 
     # Return True if any value meets the condition
-    if any(i is False for i in keysMap.values()):
+    if any(i is False for i in keys_map.values()):
         return False
     else:
         return True
