d77f74f0bc32053b130019cb3caefcf5fc452b1d
Sameh ElGazar <selgazar95@gmail.com>
2024-06-10 03:19:29 +0300

    unlock

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,6 +2,7 @@
 """Unlock the boxes"""
 
 
+#  This solution does not work for some cases
 def canUnlockAll(boxes):
     """Unlock the boxes, Problem Solving"""
     if len(boxes[0]) == 0 or type(boxes) is not list:
@@ -21,4 +22,27 @@ def canUnlockAll(boxes):
             keys_set.update(boxes[key])
 
     # Return True if any value meets the condition
-    return all(keys_map.values())
+    if any(i is False for i in keys_map.values()):
+        return False
+    else:
+        return True
+#  The right solution for all cases
+# def canUnlockAll(boxes):
+#     """
+#     Function that checks with boolean value if the list type and
+#     length to invoke two for iterations one to traverse the list
+#     and the other to compaer if key is idx or not in order to open
+#     """
+#     if type(boxes) is not list:
+#         return False
+#     elif (len(boxes)) == 0:
+#         return False
+#     for k in range(1, len(boxes) - 1):
+#         boxes_checked = False
+#         for idx in range(len(boxes)):
+#             boxes_checked = k in boxes[idx] and k != idx
+#             if boxes_checked:
+#                 break
+#         if boxes_checked is False:
+#             return boxes_checked
+#     return True
