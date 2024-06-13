98fde0076440e5979920310e5999b1f6b19f9ac3
Sameh ElGazar <selgazar95@gmail.com>
2024-06-13 18:06:44 +0300

    1

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,36 +6,42 @@ to result in exactly n H characters in the file.
 """
 
 
-def is_prime(n):
-    """
-    Check if it's prime number or not
-    """
-    not_prime = False
-    for i in range(2, n):
-        if n % i == 0:
-            not_prime = True
+# def minOperations(n):
+#     """
+#     # loop from i=2 to n and divide n by i
+#     # if mod == 0:
+#     # min = base + (n - base) / base + 1
+#     """
+#     if n <= 1:
+#         return 0
 
-    if not_prime:
-        return False
-    else:
-        return True
+#     min_op = 0
+#     for i in range(2, n):
+#         if n % i == 0:
+#             curr_op = (int(i + ((n - i) / i) + 1))
+#             if curr_op < min_op:
+#                 min_op = curr_op
 
+#     return curr_op
 
 def minOperations(n):
     """
-    # if prime number return n
-    # loop from i=2 to n and divide n by i
-    # if mod == 0:
-    # min = base + (n - base) / base + 1
+    minOperations
+    Gets fewest # of operations needed to result in exactly n H characters
     """
-    if n <= 1:
+    # all outputs should be at least 2 char: (min, Copy All => Paste)
+    if (n < 2):
         return 0
-
-    min_op = 0
-    for i in range(2, n):
-        if n % i == 0:
-            curr_op = (int(i + (n - i) / i + 1))
-            if curr_op < min_op:
-                min_op = curr_op
-
-    return curr_op
+    ops, root = 0, 2
+    while root <= n:
+        # if n evenly divides by root
+        if n % root == 0:
+            # total even-divisions by root = total operations
+            ops += root
+            # set n to the remainder
+            n = n / root
+            # reduce root to find remaining smaller vals that evenly-divide n
+            root -= 1
+        # increment root until it evenly-divides n
+        root += 1
+    return ops
