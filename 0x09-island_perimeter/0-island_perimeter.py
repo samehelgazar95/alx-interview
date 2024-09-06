4012a80c2699871d92ce9f69d800b87d8e23a990
Sameh ElGazar <selgazar95@gmail.com>
2024-09-06 12:31:04 +0300

    test

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,54 +1,79 @@
 #!/usr/bin/python3
-"""Island Perimeter - ALX Interview"""
+"""Module for Prime Game"""
 
 
-def check_val(x):
-    """_summary_
+def isWinner(x, nums):
+    """
+    Determines the winner of a set of prime number removal games.
 
     Args:
-        n (_type_): _description_
+        x (int): The number of rounds.
+        nums (list of int): A list of integers where each integer n denotes
+        a set of consecutive integers starting from 1 up to and including n.
 
     Returns:
-        _type_: _description_
+        str: The name of the player who won the most rounds (either "Ben"
+        or "Maria").
+        None: If the winner cannot be determined.
+
+    Raises:
+        None.
     """
-    if (x == 0):
-        return 1
-    return 0
+    # Check for invalid input
+    if x <= 0 or nums is None:
+        return None
+    if x != len(nums):
+        return None
+    # Initialize scores and array of possible prime numbers
+    ben = 0
+    maria = 0
+    # Create a list 'a' of length sorted(nums)[-1] + 1 with all elements
+    # initialized to 1
+    a = [1 for x in range(sorted(nums)[-1] + 1)]
+    # The first two elements of the list, a[0] and a[1], are set to 0
+    # because 0 and 1 are not prime numbers
+    a[0], a[1] = 0, 0
+    # Use Sieve of Eratosthenes algorithm to generate array of prime numbers
+    for i in range(2, len(a)):
+        rm_multiples(a, i)
+    # Play each round of the game
+    for i in nums:
+        # If the sum of prime numbers in the set is even, Ben wins
+        if sum(a[0:i + 1]) % 2 == 0:
+            ben += 1
+        else:
+            maria += 1
+    # Determine the winner of the game
+    if ben > maria:
+        return "Ben"
+    if maria > ben:
+        return "Maria"
+    return None
 
 
-def island_perimeter(grid):
-    """_summary_
+def rm_multiples(ls, x):
+    """
+    Removes multiples of a prime number from an array of possible prime
+    numbers.
 
     Args:
-        grid (_type_): _description_
+        ls (list of int): An array of possible prime numbers.
+        x (int): The prime number to remove multiples of.
+
+    Returns:
+        None.
+
+    Raises:
+        None.
     """
-
-    row = len(grid)
-    col = len(grid[0])
-    assert (1 <= row and col <= 100), "length must be between 1 an 100"
-
-    x = 0
-    for i in range(row):
-        for j in range(col):
-            assert (grid[i][j] == 0) or (grid[i][j] == 1),\
-                "grid numbers must be 0 or 1"
-            if grid[i][j] == 1:
-                if i-1 < 0:
-                    x += 1
-                else:
-                    x += check_val(grid[i-1][j])
-                if j-1 < 0:
-                    x += 1
-                else:
-                    x += check_val(grid[i][j-1])
-
-                try:
-                    x += check_val(grid[i+1][j])
-                except IndexError:
-                    x += 1
-                try:
-                    x += check_val(grid[i][j+1])
-                except IndexError:
-                    x += 1
-
-    return x
+    # This loop iterates over multiples of a prime number and marks them as
+    # non-prime by setting their corresponding value to 0 in the input
+    # list ls. Starting from 2, it sets every multiple of x up to the
+    # length of ls to 0. If the index i * x is out of range for the list ls,
+    # the try block will raise an IndexError exception, and the loop will
+    # terminate using the break statement.
+    for i in range(2, len(ls)):
+        try:
+            ls[i * x] = 0
+        except (ValueError, IndexError):
+            break
